c051124bfbfc5e92b1be9eadff25bf920a486e14
Pavel <fantasydarth031@gmail.com>
2025-10-19 17:59:03 +0600

    Основной баннер

---
+++ src/components/MainBanner.vue
@@ -0,0 +1,40 @@
+<script  lang="ts" setup="">
+
+</script>
+
+<template>
+  <div class="banner">
+    <div class="banner-img">
+      <img src="/src/assets/images/logo.webp" alt="">
+    </div>
+    <h1>Онлайн-школа по фортепиано <br> для взрослых</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.banner {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 600px;
+  background-position: center center;
+  background-attachment: scroll;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/src/assets/images/main_banner.webp");
+
+  & h1 {
+    text-align: center;
+    vertical-align: middle;
+    color: #000000;
+    font-size: 45px;
+    font-style: normal;
+    background-position: center center;
+    border-color: transparent;
+    border-style: solid;
+  }
+}
+
+</style>
