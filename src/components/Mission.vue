b332a1c6cfd39f7014b9447e854ae26282f971f1
Pavel <fantasydarth031@gmail.com>
2025-10-19 17:59:33 +0600

    блок Наша миссия

---
+++ src/components/Mission.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="mission">
+    <h3>Наша миссия:</h3>
+    <p class="text">Помочь взрослым людям исполнить свою мечту детства — научиться играть на фортепиано. Для нас важно,
+      чтобы музыка приносила вам радость и после занятий вы выходили с зарядом вдохновения, положительных эмоций и веры
+      в себя!</p>
+  </section>
+</template>
+
+<style scoped>
+.mission {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 60px 0;
+  margin: 0 auto;
+  border-width: 1px;
+  border-radius: 10px 10px 10px 10px;
+  background-color: #ffffff;
+  background-position: center center;
+  border-color: #e6e6e6;
+  border-style: solid;
+  box-shadow: 1px 1px 30px 0 rgba(175, 175, 175, 0.3);
+}
+
+h3 {
+  font-size: 36px;
+  text-align: center;
+}
+
+.text {
+  text-align: center;
+  max-width: 60%;
+}
+</style>
